05eb21fabea04cddf9e52fbf273c0ab9f043cab5
CF12 <brianx0011@gmail.com>
2017-03-15 23:48:32 -0400

    INDEV - Working on database storage using NeDB, and xp / currency system

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@
 
 // Import Libraries
 const DiscordJS = require('discord.js')
+const Nedb = require('nedb')
 const path = require('path')
 const fs = require('fs')
 const req = require('req-fast')
@@ -20,6 +21,7 @@ if (process.argv.length === 2) loadFiles()
 // Variable Declarations
 let pf
 let userCache = {}
+let db = new Nedb({filename: 'data/database.db', autoload: true})
 let categoryMappings = {
   'GENERAL': 9,
   'VIDEO-GAMES': 15,
@@ -34,10 +36,19 @@ let categoryMappings = {
 if (process.argv.length === 2) pf = config.prefix
 
 // Bot login
-if (process.argv[2].toUpperCase() === 'CI') bot.login(process.env.CI_BOT_TOKEN)
+if (process.argv.includes('CI')) bot.login(process.env.CI_BOT_TOKEN)
 else bot.login(config.bot_token)
 
 // Functions
+function loadFiles () {
+  try {
+    config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config', 'config.json')))
+    opentdbFile = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'opentdb.json')))
+  } catch (err) {
+    if (err) throw Error('Please make sure you have set up all the config files properly')
+  }
+}
+
 function checkTriviaToken (callback) {
   req({url: `https://www.opentdb.com/api.php?amount=1&token=${opentdbFile.session_token}`}, (err, res) => {
     if (err) throw Error('Error in initial OpenTDB check: Check if the service is up.')
@@ -63,20 +74,14 @@ function resetTriviaToken (callback) {
   })
 }
 
-function loadFiles () {
-  try {
-    config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config', 'config.json')))
-    opentdbFile = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'opentdb.json')))
-  } catch (err) {
-    if (err) throw Error('Please make sure you have set up all the config files properly')
-  }
-}
-
 function logChannel (channel, type, msg) {
   let embed
 
-  if (type === 'info') embed = {color: 2993972, description: `:information_source: **INFO - ** `}
-  if (type === 'err') embed = {color: 16736580, description: `:warning: **ERROR - ** `}
+  if (type === 'info') embed = {color: 2993972, description: `:information_source: **INFO - **`}
+  else if (type === 'err') embed = {color: 16736580, description: `:warning: **ERROR - **`}
+  else if (type === 'correct') embed = {color: 16736580, description: `:white_check_mark: **RESULT - **`}
+  else if (type === 'incorrect') embed = {color: 16736580, description: `:x: **RESULT - **`}
+  else if (type === 'notime') embed = {color: 16736580, description: `:alarm_clock: **TIME - **`}
   else throw Error('Invalid Channel Log')
 
   embed.description += msg
@@ -95,15 +100,51 @@ function shuffleArray (array) {
   return array
 }
 
+function dbCheckAddUser (member) {
+  db.findOne({guild_ID: member.guild.id, user_ID: member.id}, true, (err, doc) => {
+    if (err) throw err
+    if (doc === null) db.insert({guild_ID: member.guild.id, user_ID: member.id, xp: 0, money: 0})
+  })
+}
+
+function dbAddXP (member, amount) {
+  db.findOne({guild_ID: member.guild.id, user_ID: member.id}, true, (err, doc) => {
+    if (err) throw err
+    if (doc === null) return console.log(`ERROR >> User Not Found: ${member.id}`)
+
+    db.update({guild_ID: member.guild.id, user_ID: member.id}, {$set: {xp: doc.xp + amount}}, {}, (err, doc) => {
+      if (err) throw err
+    })
+  })
+}
+
+// function dbAddMoney (msgMember, amount) {
+//   let money
+//   db.findOne({guild_ID: msgMember.guild.id, user_ID: msgMember.id}, true, (err, doc) => {
+//     if (err) throw err
+//     if (doc === null) return console.log(`ERROR >> User Not Found: ${msgMember.id}`)
+//     money = doc.money
+//     console.log(doc.money)
+//     console.log(money)
+//   })
+
+//   db.update({guild_ID: msgMember.guild.id, user_ID: msgMember.id}, {$set: {'money': money += amount}}, {}, (err, doc) => {
+//     if (err) throw err
+//   })
+// }
+
 // Classes
 class TriviaQuestion {
-  constructor (diff, category, ranked) {
+  constructor (diff, category, ranked, guildMember) {
     this.category = category.toUpperCase()
     this.diff = diff.toLowerCase()
     this.reqUrl = ''
     this.question = ''
     this.choices = []
     this.type = 'UNRANKED'
+    this.author = guildMember
+    this.correct = undefined
+    this.msg
 
     if (ranked) this.type = 'RANKED'
 
@@ -111,9 +152,15 @@ class TriviaQuestion {
   }
 
   updateReqUrl () {
-    if (this.category === 'any') this.reqUrl = `https://opentdb.com/api.php?amount=1&difficulty=${diff}&type=multiple&token=${opentdbFile.session_token}`
+    if (this.category === 'any') this.reqUrl = `https://opentdb.com/api.php?amount=1&difficulty=${this.diff}&type=multiple&token=${opentdbFile.session_token}`
     else this.reqUrl = `https://opentdb.com/api.php?amount=1&category=${categoryMappings[this.category]}&difficulty=${this.diff}&type=multiple&token=${opentdbFile.session_token}`
   }
+
+  getChoices () { return this.choices }
+
+  getAuthor () { return this.author }
+
+  getMsg () { return this.msg }
 
   load () {
     this.updateReqUrl()
@@ -128,13 +175,11 @@ class TriviaQuestion {
             })
           }
 
-          console.log(res.body.results[0])
           this.question = he.decode(res.body.results[0].question)
           this.choices.push([he.decode(res.body.results[0].correct_answer), 1])
           for (let i of res.body.results[0].incorrect_answers) this.choices.push([he.decode(i), 0])
 
-          this.choice = shuffleArray(this.choices)
-          console.log(this.choices)
+          this.choices = shuffleArray(this.choices)
 
           resolve()
         } else reject(err)
@@ -142,8 +187,8 @@ class TriviaQuestion {
     })
   }
 
-  getEmbed () {
-    return {
+  send (channel, callback) {
+    channel.sendEmbed({
       color: 4833279,
       author: {
         name: `=========================❰ TRIVIA ❱=========================`
@@ -157,14 +202,18 @@ class TriviaQuestion {
         {name: `**[D] - ${this.choices[3][0]}**`, value: `=========================`}
       ],
       footer: {text: 'Powered by the Open Trivia Database: https://opentdb.com'}
-    }
+    })
+    .then((msg) => {
+      this.msg = msg
+      if (callback) callback(msg)
+    })
   }
 }
 
 // Event: When bot is ready
 bot.on('ready', () => {
   console.log('INFO >> Bot started')
-  if (process.argv[2].toUpperCase() === 'CI') {
+  if (process.argv.includes('CI')) {
     console.log('TEST >> Bot Initiation Success! Ending Process...')
     process.exit(0)
   }
@@ -176,27 +225,59 @@ bot.on('ready', () => {
 
 // Event: When the bot detects a message
 bot.on('message', (msg) => {
-  // Returns when unnecessary messages are detected
-  if (msg.author.bot || !msg.content.startsWith(pf)) return
+  // Check if message is from bot
+  if (msg.author.bot) return
 
   // Variable shortcuts
   let msgChannel = msg.channel
   let msgContent = msg.content
   let msgArray = msgContent.split(' ')
-  let msgCommand = msgArray[0].slice(pf.length).toUpperCase()
+  let msgBase = msgArray[0].toUpperCase()
   let msgArgs = msgArray.slice(1)
   let msgUser = msg.author
   let msgMember = msg.member
 
-  if (msgCommand === 'DEBUG') {
+  // Trivia answer handler
+  if (Object.keys(userCache).length !== 0 && msgMember.id in userCache) {
+    if (!['A', 'B', 'C', 'D'].includes(msgBase)) return logChannel(msgChannel, 'err', 'Invalid Answer! Please respond using the letter corresponding to your choice.')
+    let choiceIndex
+
+    if (msgBase === 'A') choiceIndex = 0
+    if (msgBase === 'B') choiceIndex = 1
+    if (msgBase === 'C') choiceIndex = 2
+    if (msgBase === 'D') choiceIndex = 3
+
+    if (userCache[msgMember.id].getChoices()[choiceIndex][1]) {
+      logChannel(msgChannel, 'correct', 'Correct!')
+      userCache[msgMember.id].msg.delete()
+      delete userCache[msgMember.id]
+      dbAddXP(msgMember, 10)
+      return
+    } else {
+      logChannel(msgChannel, 'incorrect', 'Incorrect!')
+      userCache[msgMember.id].msg.delete()
+      delete userCache[msgMember.id]
+      dbAddXP(msgMember, -10)
+      return
+    }
+  }
+
+  // Returns when unnecessary messages are detected
+  if (!msg.content.startsWith(pf)) return
+  msgBase = msgBase.slice(pf.length)
+
+  // Command: Debug
+  if (msgBase === 'DEBUG') {
     let tq = new TriviaQuestion('easy', 'math')
     tq.load().then(() => {
-      console.log(tq.getEmbed())
-      msgChannel.sendEmbed(tq.getEmbed())
+      console.log(tq.send())
+      msgChannel.sendEmbed(tq.send())
     }, (err) => { if (err) console.log('ERROR >> Error in OpenTDB request') })
   }
 
-  if (msgCommand === 'TRIVIA') {
+  // Command: Main Trivia Command
+  if (msgBase === 'TRIVIA') {
+    dbCheckAddUser(msgMember)
     if (msgArgs.length === 0) return logChannel(msgChannel, 'err', `Invalid usage! Use **${pf}trivia help** for more info.`)
     if (msgArgs.length >= 1) {
       if (msgArgs[0] === 'unranked') {
@@ -204,15 +285,20 @@ bot.on('message', (msg) => {
         if (!['EASY', 'MEDIUM', 'HARD'].includes(msgArgs[1].toUpperCase())) return logChannel(msgChannel, 'err', `Invalid difficulty! Valid difficulties: **Easy, Medium, Hard**`)
         if (!Object.keys(categoryMappings).includes(msgArgs[2].toUpperCase())) return logChannel(msgChannel, 'err', `Invalid category! To get a list of all available categories, please use **${pf}trivia categories**`)
 
-        let tq = new TriviaQuestion(msgArgs[1], msgArgs[2])
+        let tq = new TriviaQuestion(msgArgs[1], msgArgs[2], false, msgMember)
 
-        if (!Object.keys(userCache).inclues(msgMember.id)) userCache.msgMember.id = tq
+        if (!Object.keys(userCache).includes(msgMember.id)) userCache[msgMember.id] = undefined
+        else if (typeof userCache[msgMember.id] === 'function') return logChannel(msgChannel, 'err', `Please answer your current trivia question!`)
+        userCache[msgMember.id] = tq
 
         tq.load().then(() => {
-          msgChannel.sendEmbed(tq.getEmbed())
-          .then((msg) => {
+          tq.send(msgChannel, (msg) => {
             setTimeout(() => {
+              if (!userCache[msgMember.id] || userCache[msgMember.id].getMsg().id !== msg.id) return
+
               msg.delete()
+              delete userCache[msgMember.id]
+              return logChannel(msgChannel, 'notime', 'Time\'s up!')
             }, 10000)
           })
         })
@@ -221,7 +307,7 @@ bot.on('message', (msg) => {
     return
   }
 
-  if (msgCommand === 'INFO') {
+  if (msgBase === 'INFO') {
     let imgCredits = [
       {name: 'Bot Icon', value: '*Icon made by Dimi Kazak from www.flaticon.com*'}
     ]
